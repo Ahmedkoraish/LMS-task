39c8e9145d7ccae253ec1c09db0fe2d677d4ddb3
Ahmed koraish <136814559+Ahmedkoraish@users.noreply.github.com>
2025-11-06 11:47:47 +0200

    style: apply MUI for component styling

---
+++ src/Theme/theme.js
@@ -0,0 +1,15 @@
+import {createTheme} from "@mui/material";
+
+const theme = createTheme({
+    palette:{
+        primary:{
+            main:'#3f63f4'
+        },
+        secondary:{
+            main:'#020222'
+        }
+    }
+})
+
+
+export default theme
