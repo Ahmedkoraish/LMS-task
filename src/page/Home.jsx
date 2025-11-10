236489af84cd7722b08f49854f1e229fd425a562
Ahmed koraish <136814559+Ahmedkoraish@users.noreply.github.com>
2025-11-10 12:22:04 +0200

    add subscribe button

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -5,6 +5,8 @@ import Course from '../component/Course'
 import Articals from '../component/Articals'
 import Subscripe from '../component/Subscripe'
 import Nav from '../component/Nav'
+import { Button, IconButton } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Home() {
     return (
@@ -15,6 +17,12 @@ export default function Home() {
             <Course />
             <Articals />
             <Subscripe />
+            <Button variant="contained" color="primary" sx={{ position: "fixed", bottom: 20, right: 20, px: 2, py: 1, borderRadius: 6 }}>
+                <IconButton >
+                    <AddIcon sx={{ color: "#fff" }}/>
+                </IconButton>
+                Subscribe
+            </Button>
         </>
     )
 }
